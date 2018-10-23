6c260fe399e542b364a67c3cb3f7b1969ba3c84e
GermainPinganaud <germain.pinganaud@gmail.com>
2018-10-23 17:39:58 +0200

    input v1, text recuperer et 'schema > ' ajouté avant le 4eme charactere de l'input

--- src/input.js
+++ src/input.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   $('#royaume').hide();
+
   $('#input-button').click(function() {
-    $('#royaume').show(400);
-    alert($('#input-field').attr('value'));
+    let output = "";
+    input = $('#input-field').val();
+    inputCharList = input.split('');
+    for (let char = 0; char < inputCharList.length; char ++) {
+      output += inputCharList[char];
+      if (char == 2) {
+        output += '#schema > ';
+      }
+    }
+    eval(output);
   });
 });
